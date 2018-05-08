87268ccaf96b2520e90b8f16d3b9ebe7b1ae79f6
Леонид Казаков <kazakovlv@mail.ru>
2018-05-08 12:04:37 +0300

    sprite.svg

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ gulp.task("sprite", function() {
       inlineSvg: true
     }))
     .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(gulp.dest("source/tmp"));
 });
 
 gulp.task("html", function() {
