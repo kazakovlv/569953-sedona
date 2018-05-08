7f4e626cb04f74085a03a2ddf6ab9c3823b85662
Леонид Казаков <kazakovlv@mail.ru>
2018-05-08 11:42:14 +0300

    imagemin 1

--- gulpfile.js
+++ gulpfile.js
@@ -54,16 +54,16 @@ gulp.task("images", function() {
     .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("build/img"));
-});
-
 gulp.task("svgo", function() {
   return gulp.src("source/img/*.svg")
       .pipe(svgo())
       .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("sprite", function() {
